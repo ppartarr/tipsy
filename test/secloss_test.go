a1282d1a692887060fe26fdc426db562f3529632
Philippe Partarrieu <philippe@partarrieu.me>
2021-01-28 10:11:23 +0100

    add optimal checker secloss test

--- test/secloss_test.go
+++ test/secloss_test.go
@@ -41,8 +41,8 @@ func TestSecLossAlways(t *testing.T) {
 	q := 10
 	ballSize := 3
 	maxPasswordLength := 6
-	attackerListFile := "../data/rockyou-1000-withcount.txt"
-	defenderListFile := "../data/rockyou-1000-withcount.txt"
+	attackerListFile := "../data/rockyou-withcount.txt"
+	defenderListFile := "../data/rockyou-withcount.txt"
 
 	// convert results to json
 	result := greedyMaxCoverageHeap(server, q, ballSize, maxPasswordLength, attackerListFile, defenderListFile)
@@ -92,8 +92,60 @@ func TestSecLossBlacklist(t *testing.T) {
 	q := 10
 	ballSize := 3
 	maxPasswordLength := 6
-	attackerListFile := "../data/rockyou-1000-withcount.txt"
-	defenderListFile := "../data/rockyou-1000-withcount.txt"
+	attackerListFile := "../data/rockyou-withcount.txt"
+	defenderListFile := "../data/rockyou-withcount.txt"
+
+	// convert results to json
+	result := greedyMaxCoverageHeap(server, q, ballSize, maxPasswordLength, attackerListFile, defenderListFile)
+	bytes, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	// save json to file
+	filename := strconv.Itoa(q) + "-" + strconv.Itoa(ballSize) + "-" + strconv.Itoa(maxPasswordLength) + ".json"
+	err = ioutil.WriteFile(filepath.Join(checker, filename), bytes, 0666)
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func TestSecLossOptimal(t *testing.T) {
+	server := &config.Server{
+		Checker: &config.Checker{
+			Optimal: &config.OptimalChecker{
+				File:                    "data/rockyou-1k.txt",
+				QthMostProbablePassword: 10,
+			},
+		},
+
+		Typos: map[string]int{
+			"same":          90234,
+			"other":         1918,
+			"swc-al":        169,
+			"kclose":        1385,
+			"keypress-edit": 1000,
+			"rm-last":       382,
+			"swc-first":     209,
+			"rm-first":      55,
+			"sws-last":      19,
+			"tcerror":       18,
+			"sws-lastn":     14,
+			"upncap":        13,
+			"n2s-last":      9,
+			"cap2up":        5,
+			"add1-last":     5,
+		},
+
+		Correctors: []string{"swc-all", "rm-last", "swc-first"},
+	}
+
+	checker := "optimal"
+	q := 10
+	ballSize := 3
+	maxPasswordLength := 6
+	attackerListFile := "../data/rockyou-1m-withcount.txt"
+	defenderListFile := "../data/rockyou-1m-withcount.txt"
 
 	// convert results to json
 	result := greedyMaxCoverageHeap(server, q, ballSize, maxPasswordLength, attackerListFile, defenderListFile)
